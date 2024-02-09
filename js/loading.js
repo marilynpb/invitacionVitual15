bbf02f0e037bf44b5ec2f549ff8b8c89c628c48c
marilynpb <g.marilyn182@hotmail.com>
2024-02-09 17:01:47 -0300

    Add loading

---
+++ js/loading.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Espera 10 segundos y luego redirige al home
+  setTimeout(function() {
+    window.location.href = "../tarjeta.html";
+  }, 5000); // 10 segundos en milisegundos
+});
